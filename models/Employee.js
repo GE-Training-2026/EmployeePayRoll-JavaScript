a187b26c3d35fcb6a9c6113104412f302e5e3688
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-18 20:24:25 +0530

    [Jay]: feat(model): define Employee DTO class for structured data representation

---
+++ models/Employee.js
@@ -0,0 +1,10 @@
+class Employee {
+  constructor(id, name, salary, department) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+    this.department = department;
+  }
+}
+
+module.exports = Employee;
